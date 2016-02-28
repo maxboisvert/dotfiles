0a34ee7e0a7f784d3ecbba4ba8d1f1e5529ccce2
Maxime Boisvert <boisvertmaxime@gmail.com>
2016-02-28 13:02:54 -0500

    Moved home file sin a folder

--- link-to-home.sh
+++ link-to-home.sh
@@ -1,11 +1,14 @@
 
-# Use at your own risk. This will override your home with the stuff in this folder.
+# Use at your own risk. This will override your home with the (cool) stuff in this folder.
 
 set -e
 
-for file in $(find $(pwd) -maxdepth 1)
+for file in $(find $(pwd)/home -maxdepth 1 -mindepth 1)
 do
-	rm -f ~/$(basename $file)
+	HOME_FILE=~/$(basename $file)
+	if [ -e $HOME_FILE ] ; then
+		find $HOME_FILE -type l | xargs rm
+	fi
 	ln -s $file ~
 done
 
